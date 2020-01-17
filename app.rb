f2842e893d359fc264f80629148b95448d3bc202
Roshanak Agahi <rosh.agahi@gmail.com>
2020-01-17 20:48:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -32,7 +32,11 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do 
+    @operation = params[:operation]
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
     
+    case 
   end
 
 end
