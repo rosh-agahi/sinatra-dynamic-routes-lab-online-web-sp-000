899d2e9c07cdae6d0c504976ab4174a49ffabed5
Roshanak Agahi <rosh.agahi@gmail.com>
2020-01-17 20:55:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,8 +17,9 @@ class App < Sinatra::Base
     @number = params[:number].to_i
     @phrase = params[:phrase]
     
-    @number.times {puts "#{@phrase}"}
-    
+    @number.times do
+      puts @phrase
+    end
   end 
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do 
