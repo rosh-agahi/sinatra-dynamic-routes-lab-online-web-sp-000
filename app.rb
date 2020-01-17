56670f57134a06f09d1415076c6ad8c006b7c842
Roshanak Agahi <rosh.agahi@gmail.com>
2020-01-17 20:37:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,13 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  
+  get '/reversename/:name'
+  
   get '/square/:number' do
   end
   
   get '/say/:number/:phrase'
   end 
 
-
+  
 
 
 
